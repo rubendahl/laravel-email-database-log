fe0f78e7910e86d2dbabc4703a0441fbb125cf4d
Kent Dahl <kent@rubendahl.no>
2019-02-06 15:30:39 +0100

    Separate webhook prefix.

--- config/email_log.php
+++ config/email_log.php
@@ -4,6 +4,7 @@ return [
     'folder' => env('EMAIL_LOG_ATTACHMENT_FOLDER','email_log_attachments'),
     'access_middleware' => env('EMAIL_LOG_ACCESS_MIDDLEWARE',null),
     'routes_prefix' => env('EMAIL_LOG_ROUTES_PREFIX',''), //when changing prefix please be sure to update the webhook's URLs also
+    'routes_webhook_prefix' => env('EMAIL_LOG_ROUTES_WEBHOOK_PREFIX', env('EMAIL_LOG_ROUTES_PREFIX','')), //when changing prefix please be sure to update the webhook's URLs also
     'mailgun' => [
         'secret' => env('MAILGUN_SECRET', null),
         'filter_unknown_emails' => env('EMAIL_LOG_MAILGUN_FILTER_UNKNOWN_EMAILS', true),
