b2fe54b80d15e41b3083156c4df168a849fc80b5
Kent Dahl <kent@rubendahl.no>
2019-02-06 13:09:54 +0100

    Do not use env from code due to config caching.

--- config/email_log.php
+++ config/email_log.php
@@ -4,6 +4,9 @@ return [
     'folder' => env('EMAIL_LOG_ATTACHMENT_FOLDER','email_log_attachments'),
     'access_middleware' => env('EMAIL_LOG_ACCESS_MIDDLEWARE',null),
     'routes_prefix' => env('EMAIL_LOG_ROUTES_PREFIX',''), //when changing prefix please be sure to update the webhook's URLs also
+    'mailgun' => [
+        'secret' => env('MAILGUN_SECRET', null),
+    ],
     'save_attachments' => env('EMAIL_LOG_SAVE_ATTACHMENT','folder'),
     'store_user_id' => env('EMAIL_LOG_STORE_USER_ID',false),
 ];
